9a0c33bc1894c53bcebf2369cc90a421e9c1a58a
raphatapi <raphaeltapioca10@gmail.com>
2020-04-01 14:35:59 -0600

    added sparkles

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ $(document).ready(function(){
     });
 
     $(".star").sparkle({
-        color: "#ff00ff"
+        color: "#ff00ff",
+        count: 40,
+        overlap: 20,
+        speed: 1.2,
+        minSize: 2,
+        maxSize: 8
     });
 });
